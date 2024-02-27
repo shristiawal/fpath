5bdf534b026f17c919960d0d5b9fafc9c3761f6a
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-27 13:56:36 +0545

    pug

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine','pug');
+app.set('views','views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
@@ -17,4 +21,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
-app.listen(3000);
+app.listen(3002);
